96886a5b2631e0ea535f4cfe5b57834445e199ee
yijerjer <loh.yijer@live.com>
2017-02-04 22:52:01 +0800

    able to print on page validation errors

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,5 +1,13 @@
 get '/' do
   # let user create new short URL, display a list of shortened URLs
+  if params["error"] == "is invalid"
+    @error_msgs = "Invalid Address"
+  elsif params["error"] == "has already been taken"
+    @error_msgs = "A short link has already been created for this URL"
+  else
+    @error_msgs = nil
+  end
+
   @array = Url.all
   erb :"static/index"
 end
@@ -8,11 +16,12 @@ post '/' do
   # create a new Url
   new_url = Url.new(long_url: params[:long_url])
   if new_url.invalid?
-    @error_msgs = new_url.errors.messages[:long_url]
+    error_msgs = new_url.errors.messages[:long_url][0]
+    redirect "/?error=#{error_msgs}"
   else
     new_url.save
+    redirect "/"
   end
-  redirect "/"
 end
 
 # i.e. /q6bda
